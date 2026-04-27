98fe0cedcc3e0a461a7cc48c318a95497d39a6b2
saipavangit21 <114813868+saipavangit21@users.noreply.github.com>
2026-04-27 11:08:42 +0200

    Fix CORS: allow ngrok-skip-browser-warning header in preflight

--- proxy/app.py
+++ proxy/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.after_request
 def add_cors(response):
     response.headers["Access-Control-Allow-Origin"]  = "*"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, ngrok-skip-browser-warning"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
     return response
 
